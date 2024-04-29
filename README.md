71924fe3ce59c5756f283f54fdc82b74ed2324f3
Kaustav Banerjee <kaustav@opencraft.com>
2024-04-29 17:31:55 +0530

    feat: initial implementation (#1)
    * feat: initial implementation
    
    * fix: address review comments

--- README.md
+++
@@ -1,2 +0,0 @@
-# xblock-video-iframe
-XBlock to embed video content using iframe along with download buttons
---
+++ video_iframe/__init__.py
@@ -0,0 +1,7 @@
+"""
+Init for the VideoIframeXBlock package.
+"""
+
+from .video_iframe import VideoIframeXBlock
+
+__version__ = '0.1.0'
